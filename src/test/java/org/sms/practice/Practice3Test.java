90bc72d2074b8682b506011f8d25b44cc7730b12
SIVA KUMAR <SIVA KUMAR@DESKTOP-R83QAQH>
2022-09-23 16:06:13 +0530

    adding browser and url

--- src/test/java/org/sms/practice/Practice3Test.java
+++ src/test/java/org/sms/practice/Practice3Test.java
@@ -3,10 +3,15 @@ package org.sms.practice;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
+import io.opentelemetry.exporter.logging.SystemOutLogExporter;
+
 public class Practice3Test {
 	@Test
 	public void step3Test()
-	{
+	{String browser = System.getProperty("b");
+	String url=System.getProperty("url");
+			System.out.println(browser);
+	System.out.println(url);
 		Reporter.log("3",true);
 		Reporter.log("4",true);
 	}
